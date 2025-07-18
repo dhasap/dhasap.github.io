c088c75a2be63152e6ab790eacd33580060627f8
dhasap <87491587+dhasap@users.noreply.github.com>
2025-07-19 01:33:02 +0700

    Update main.js

--- kelas-odgj/js/main.js
+++ kelas-odgj/js/main.js
@@ -1,1 +1,14 @@
+// Animasi Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
+// Fitur Dark Mode (Opsional)
+const darkModeToggle = document.createElement('button');
+darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+document.body.prepend(darkModeToggle);
